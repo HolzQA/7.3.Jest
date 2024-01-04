827759a1123eeeafb2b7e40d6b0f4a9b2534d747
Marija Holzeimer <diatmin@gmail.com>
2024-01-05 02:15:21 +0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100
+      }
+    },
+    collectCoverageFrom: [
+        '**/*.{js,jsx}',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+        '!**/jest.config.js /**'
+    ]
+  };
+  
+  module.exports = config;
